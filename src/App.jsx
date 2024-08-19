f079b10bc117b8758b5ffd4cd13075750d79c44b
jacomarr <jacobmarchionda@tutanota.com>
2024-08-18 23:31:31 -0400

    user auth things

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
-import reactLogo from "./assets/react.svg";
 import "./App.css";
+import { Amplify } from "aws-amplify";
+import { generateClient } from "aws-amplify/data";
+import outputs from "../amplify_outputs.json";
+
+Amplify.configure(outputs);
+const client = generateClient({
+    authMode: "userPool",
+});
 
 function App() {
     return (
         <div className="App">
             <header className="App-header">
-                <img src={reactLogo} className="logo react" alt="React logo" />
-
-                <h1>Hello from Amplify</h1>
+                <p>{ client.models.User.get() }</p>
             </header>
         </div>
     );
