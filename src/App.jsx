5e3acc385f62a36039195e8f40efd702ea361920
jacomarr <jacobmarchionda@tutanota.com>
2024-09-09 21:13:07 -0400

    login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,26 @@
 
-import { Authenticator } from '@aws-amplify/ui-react';
+import { Authenticator,
+    Flex
+ } from '@aws-amplify/ui-react';
 import { Amplify } from 'aws-amplify';
 import outputs from '../amplify_outputs.json';
 import '@aws-amplify/ui-react/styles.css';
+import { ThemeProvider } from '@aws-amplify/ui-react';
+import theme from './theme';
 
 Amplify.configure(outputs);
 
-export default function App({ Component, pageProps }) {
+export default function App() {
   return (
-    <Authenticator>
-      {({ signOut, user }) => (
-        <main>
-          <h1>Hello {user?.username}</h1>
-          <button onClick={signOut}>Sign out</button>
-          <Component {...pageProps} />
-        </main>
-      )}
-    </Authenticator>
+        <ThemeProvider theme={theme}>
+            <Authenticator>
+            {({ signOut, user }) => (
+                    <main>
+                        <h1>Hello {user?.username}</h1>
+                        <button onClick={signOut}>Sign out</button>
+                    </main>
+            )}
+            </Authenticator>
+        </ThemeProvider>    
   );
 };
