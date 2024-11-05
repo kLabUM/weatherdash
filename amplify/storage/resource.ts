493b92c75ab0b665641038866852862f43194b6b
jacomarr <jacobmarchionda@tutanota.com>
2024-11-04 22:50:45 -0500

    :/

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -3,7 +3,7 @@ import { defineStorage } from '@aws-amplify/backend';
 export const storage = defineStorage({
   name: 'weatherdashResources',
   access: (allow) => ({
-    '*': [
+    '/*': [
       allow.authenticated.to(['read'])
     ]
   })
