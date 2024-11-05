5ea13e780c45e381c2e8a289d7d67060836ccfdb
jacomarr <jacobmarchionda@tutanota.com>
2024-11-04 22:15:55 -0500

    added authorization for users to access storage

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -1,5 +1,10 @@
 import { defineStorage } from '@aws-amplify/backend';
 
 export const storage = defineStorage({
-  name: 'weatherdashResources'
+  name: 'weatherdashResources',
+  access: (allow) => ({
+    'media/*': [
+      allow.authenticated.to(['read'])
+    ]
+  })
 });
