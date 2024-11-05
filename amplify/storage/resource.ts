5c17d89bfeda44d5b0557ba59cc83a31f6c2738a
jacomarr <jacobmarchionda@tutanota.com>
2024-11-05 14:43:43 -0500

    auth issues

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -3,7 +3,7 @@ import { defineStorage } from '@aws-amplify/backend';
 export const storage = defineStorage({
   name: 'weatherdashResources',
   access: (allow) => ({
-    '/*': [
+    '/media/*': [
       allow.authenticated.to(['read'])
     ]
   })
